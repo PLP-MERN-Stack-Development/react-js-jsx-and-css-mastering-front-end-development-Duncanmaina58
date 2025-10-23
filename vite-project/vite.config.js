1066f5b5e311eae82f97939a30355eb4dd29bf6b
Duncanmaina58 <dun.can.duntez@gmail.com>
2025-10-23 11:54:21 +0300

    Final project setup and configuration for GitHub Pages deployment

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
+
+// const repoName = 'react-js-jsx-and-css-mastering-front-end-development-Duncanmaina58';
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
   ],
+  base: `/react-js-jsx-and-css-mastering-front-end-development-Duncanmaina58/`,
 })
